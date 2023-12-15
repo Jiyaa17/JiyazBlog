a8cd2d89d146af03def72426501813d495d9e8e1
Jiyaa17 <132640422+Jiyaa17@users.noreply.github.com>
2023-12-15 17:06:24 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+
+var clickerButton = document.getElementById("clicker");
+var onButtonClick = function() {
+clickerButton= window.location.href ="My art.html";
+};
+clickerButton.addEventListener("click", onButtonClick);
+var clickerButton2 = document.getElementById('clicker2');
+var onButtonClick2 = function(){
+    clickerButton2 = window.location.href ="Memories.html";
+}
+clickerButton2.addEventListener('click', onButtonClick2)
+
+var clickerbutton3 = document.getElementById("clicker3");
+var onbuttonclick3 =function(){
+    clickerbutton3 = window.location.href = "Dreams.html";
+} ;
+clickerbutton3.addEventListener("click", onbuttonclick3);
+
+var clickerbutton4 = document.getElementById("clicker4");
+var onbuttonclick4 =function(){
+    clickerbutton4 = window.location.href = "Binge It!.html";
+};
+clickerbutton4.addEventListener("click", onbuttonclick4);
+
+var clickerbutton5 = document.getElementById("clicker5");
+var onbuttonclick5 =function(){
+    clickerbutton5 = window.location.href = "About me.html";
+} ;
+clickerbutton5.addEventListener("click", onbuttonclick5);
